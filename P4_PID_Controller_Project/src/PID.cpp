953c731e19850d1be37d2b6cf6d179350d936ece
AkashMetawala <akashmetawala@gmail.com>
2018-04-15 13:03:35 -0400

    Adding all files for PID project

---
+++ P4_PID_Controller_Project/src/PID.cpp
@@ -0,0 +1,31 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  this->d_error = 0;
+  this->i_error = 0;
+}
+
+void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
+}
+
+double PID::TotalError(double speedFactor) {
+  return (-(Kp*p_error*speedFactor) - (Kd*d_error) - (Ki*i_error*(1-speedFactor)));
+}
+
